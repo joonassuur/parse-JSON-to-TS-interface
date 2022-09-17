29fc1ac3a9138033a43710aca1147e1a0c4268ef
Joonas <joonas.suurvali@gmail.com>
2022-09-17 20:45:14 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const getValueType = (value: any) => {
+  if (typeof value === 'object') {
+    const stringified = JSON.stringify(value);
+    if (stringified.includes('[')) {
+      return '[]';
+    }
+    if (stringified.includes('{')) {
+      return 'Record<string, unknown>';
+    }
+    if (stringified.includes('null')) {
+      return 'null';
+    }
+  }
+  return typeof value;
+};
+
+const parseJsonIntoInterface = (inputJson: string) => {
+  const parsedJson = JSON.parse(inputJson);
+  let outputInterfaceString = '';
+
+  Object.entries(parsedJson).map(([key, value]) => {
+    const valueType = getValueType(value);
+    const lastIndexOfSemicolon = outputInterfaceString.lastIndexOf(';');
+
+    if (lastIndexOfSemicolon === -1) {
+      outputInterfaceString = `{ ${key}: ${valueType}; }`;
+    } else {
+      const firstPart = outputInterfaceString.slice(
+        0,
+        lastIndexOfSemicolon + 1
+      );
+      const addendum = `${key}: ${valueType};`;
+      const result = `${firstPart} ${addendum} }`;
+      outputInterfaceString = result;
+    }
+    return false;
+  });
+  const completedInterface = `interface SampleOutputInterface ${outputInterfaceString}`;
+  const lineBreakAfterFirstBrace = completedInterface.replace('{', '{\n');
+  const stringWithLineBreaks = lineBreakAfterFirstBrace.replaceAll(';', ';\n');
+
+  return stringWithLineBreaks;
+};
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+
+  const handleJsonParse = () => {
+    if (inputText) {
+      try {
+        const parsedString = parseJsonIntoInterface(inputText);
+        setOutputText(parsedString);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex' }}>
+      <textarea
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        rows={10}
+      />
+      <button onClick={handleJsonParse}>Parse</button>
+      <div
+        style={{
+          whiteSpace: 'pre',
+        }}
+      >
+        {outputText}
+      </div>
     </div>
   );
 }
